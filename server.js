a004daf8204184b502942048bc24722141627c93
lVovaNl <vladimir.shlyakhovskiy>
2017-07-19 14:14:59 +0300

    final

--- server.js
+++ server.js
@@ -4,11 +4,21 @@ var express = require('express');
 // создаём Express-приложение
 var app = express();
 
+app.use(express.static('calculator'));
 // создаём маршрут для главной страницы
 // http://localhost:8080/
 app.get('/', function(req, res) {
-  res.sendfile('calcul.html');
+  res.sendfile('index.html');
 });
 
+app.get('/', function(req, res) {
+  res.sendfile('calculator.css');
+});
+
+app.get('/', function(req, res) {
+  res.sendfile('calculator.js');
+});
+
+
 // запускаем сервер на порту 8080
-app.listen(8080);
+app.listen(8080, '192.168.0.121');
